ec97906900220448dca97583c383291da8eae846
Reanna Bain <relynbain@gmail.com>
2015-10-01 23:37:10 -0500

    search function working, click to details view working

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -8,35 +8,31 @@ var $ = require('jquery'),
 
 	console.log('loaded dist file')
 
-// var homeURL = 'https://openapi.etsy.com/v2/listings/active.js?api_key=3w2bktapp0baml9j70tm7rca'
-// 	detailsURL = 'https://openapi.etsy.com/v2/listings/' + 'uniqueID' + '.js?api_key=3w2bktapp0baml9j70tm7rca'
-// 	searchURl = 'https://openapi.etsy.com/v2/listings/active.js?api_key=3w2bktapp0baml9j70tm7rca' + '&keywords='
-// 	artistWork = 'https://openapi.etsy.com/v2/shops/' + ':shop_id' + '/listings/active.js?api_key=3w2bktapp0baml9j70tm7rca'
-
 
 //--------------COLLECTION----------------------
 var EtsyCollection = Backbone.Collection.extend({
 	
-	url: 'https://openapi.etsy.com/v2/listings/active.js?api_key=3w2bktapp0baml9j70tm7rca&callback=?',
+	url: 'https://openapi.etsy.com/v2/listings/active.js',
+	apiKey:'3w2bktapp0baml9j70tm7rca',
 		
 	parse: function(responseData){
 		var startingArray = responseData.results
+		console.log(responseData)
 		return startingArray
 	}
-}),
-
-ets = new EtsyCollection
-
-ets.fetch().done(function(){
-	console.log(ets)
 })
 
-.then(function(data){
-console.log("and then")
-console.log(data)})
-
-
-
+//------------MODEL(for Details View)-----------
+var EtsyModel = Backbone.Model.extend({
+	
+	url: 'https://openapi.etsy.com/v2/listings',
+	apiKey:'3w2bktapp0baml9j70tm7rca',
+		
+	parse: function(responseData){
+		var singleListing = responseData.results[0]
+		return singleListing
+	}
+})
 
 //-----------------VIEWS-----------------------
 
@@ -46,22 +42,13 @@ var HomeView = Backbone.View.extend({
 
 	events:{
 		"keypress input": "getUserQuery",
-		"click .images": "getDetailsView"
+		"click img": "getDetailsView"
 	},
-	
-	render: function(){
-		console.log(this)
-		var	etsyArray = this.collection.models.slice(0,20)
 
-		var htmlString = ""
-			etsyArray.forEach((obj)=>{
-			htmlString += `<div class="images"><img src= ${obj.attributes.MainImage.url_170x135}></div>`
-			})
-		
-		var title = `<p>Welcome to Etsy</p>`,
-			input = `<input type="text" placeholder="Search Etsy!"></input>`
-
-		this.$el.html(title + input + htmlString)
+	getDetailsView: function(event){
+		var detailClick = event.target,
+		detailId = detailClick.getAttribute("id")
+		location.hash = 'details/' + detailId
 	},
 
 	getUserQuery: function(event){
@@ -71,94 +58,172 @@ var HomeView = Backbone.View.extend({
 				keywords = inputEl.value
 			location.hash = `search/${keywords}`
 		}
-	}
-
-// 	getDetailsView: function() {
-// 		var imgEl = event.target,
-// 			id = imgEl.value
-// 		location.hash = `details/${id}`
-// 	}
-// })
-
-// var DetailsView = Backbone.View.extend({
-// 	el: "#container",
-
-// 	getProductInfo: function(product){
-// 		var htmlString_details = `
-// 		<div id='details view'>
-// 				<p> Title: ${product.attributes.title}</p>
-// 				<p> Description: ${product.attributes.description}</p>
-// 				<img src= ${product.attributes.MainImage.url_170x135}>
-
-// 		</div>`
-// 		return htmlString_details
-// 	},
+	},
 	
+	render: function(){
+		console.log(this)
+		var	etsyArray = this.collection.models.slice(0,20)
 
-
-// 	render: function(){
-// 		console.log('pulling up details view')
+		var htmlString = ""
+			etsyArray.forEach((obj)=>{
+			htmlString += `<div class="images"><img id="${obj.attributes.listing_id}" src="${obj.attributes.MainImage.url_170x135}"></div>`
+			})
 		
-// 		var etsyArray = this.collection.models
-// 		var htmlString = ""
-// 			etsyArray.forEach((obj)=>{
-// 				htmlString += this.getProductInfo(obj)
-// 			})
-// 	}
+		var title = `<p id="title">Welcome to Etsy</p>`,
+			input = `<input type="text" placeholder="Search Etsy!"></input>`
 
+		this.$el.html(title + input + htmlString)
+		$('img').click(this.getDetailsView.bind(this))
+	}
 })
 
 
+var DetailsView = Backbone.View.extend({
+	el: "#container",
+
+	render: function(){
+		// console.log('this is detail view')
+
+		var e = this.model.attributes,
+			title = e.title,
+			image = e.MainImage.url_570xN,
+			price = e.price,
+			quantity = e.quantity,
+			description = e.description	
+
+		
+		console.log(e)
+
+		this.$el.html(`<h1>${title}</h1>
+			<div id="singleImage"><img src = ${image}></div>
+			<p class="des">$${price} USD, Quantity: ${quantity}</p>
+			<p>${description}</p>`)
 
 
+	}
 
-// initialize: function(){
-// 		this.listenTo(this.model, 'update', this.render)
-// 	}
-// })
+})
 
 
 
 //-----------------ROUTER-----------------------
 var EtsyRouter = Backbone.Router.extend({
+	
 	routes:{
 		
 		'search/:keywords': 'showSearch',
-		'*anyroute/:home': 'showHomeScreen'
-		// 'details/:id': 'showDetailsScreen'
+		'details/:id': 'showDetailsScreen',
+		'*anyroute': 'showHomeScreen'
 	},
 
 	showHomeScreen: function(){
 		console.log('showing default')
-		this.ets.fetch({
-			processData: true
+		var self = this
+		this.ec.fetch({
+			data: {
+				api_key: this.ec.apiKey,
+				includes: 'MainImage'
+			},
+			processData: true,
+			dataType: 'jsonp'
+		}).done(function(){
+			self.hv.render()
 		})
 	},
 
-	showSearch: function(keywords){
+	//above: ajax doing string formatting for us; q is the key; depends on API; when doing a search the value is q. Query is what we are placing into the route
+
+
+	showSearch: function(keyword){
 		console.log('showing whatcha searched for')
-		this.ets.category_path[0] = keywords
-		this.ets.fetch({
-			processData:true
-		})
-	},
+		var self = this
 
-	showDetailsScreen: function(id){
+		this.ec.fetch({
+			data: {
+				keywords: keyword,
+				api_key: this.ec.apiKey,
+				includes: 'MainImage'
+			},
+			processData: true,
+			dataType:'jsonp'
+		}).done(function(){
+			self.hv.render()})
+		},
+
+	showDetailsScreen: function(listing_id){
 		console.log('showing your details!')
+		var self = this
+		
+		this.em.fetch({
+			url: `${this.em.url}/${listing_id}/.js`,  
+			data:{
+				api_key: this.em.apiKey,
+				includes: 'MainImage'
+			},
+			processData: true,
+			dataType: 'jsonp'
+		}).done(function(){
+				self.dv.render()})
 	},
+		
 
 	initialize: function(){
-		this.ets = new EtsyCollection()
+		this.ec = new EtsyCollection
 		this.hv = new HomeView({collection:this.ec})
+		this.em = new EtsyModel
+		this.dv = new DetailsView({model:this.em})
 		Backbone.history.start()
 	}
 
 })
 
-var hv = new HomeView({collection: ets})
+var newRoute = new EtsyRouter
 
-ets.fetch({
-	data:{
-		includes: 'MainImage'
-	}}).done(hv.render.bind(hv))
+// var hv = new HomeView({collection: ec})
+
+
+
+
+// var myModel = $.get('url')
+
+// bb.model = {
+
+// 	data:mymodel,
+
+// 	get:function(query){
+
+// 		bb.trigger('d')
+
+// 		return myModel.e.prop
+// 	},
+// 	set:function(key , val){
+
+// 		bb.trigger('validate' , val)
+
+// 	 	myModel[query] = key
+// 	 	return key
+// 	} ,
+
+// 	validate:function(userF){
+
+// 		bb.listenTo('validate' , function(val){
+// 			userF.call(this , val)
+// 		})
+		
+// 	}
+
+
+// }
+// var e=myModel.prop
+// bb.get('e.prop')
+// bb.data.prop
+// bb.validate(function(phoneNum) {
+// 	if(phoneNum.match(/[^0-9]/gi))
+// 		return false;
+// 	return true;	
+// })
+
+
+
+
 
